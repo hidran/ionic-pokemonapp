363b58e221bced441b2cc66d7f6d57e842d37024
Hidran Arias <hidran.arias@spryker.com>
2022-10-30 02:27:46 +0200

    Riproduci 33. Creare le Interface IResult e IPokemon

--- src/app/services/pokemon-api.service.ts
+++ src/app/services/pokemon-api.service.ts
@@ -1,6 +1,12 @@
+import { Pokemon } from '../models/Pokemon';
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
+import { IPokemon, IResult } from '../interfaces/ipokemons';
+import { switchMap, tap, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,9 +14,12 @@ export class PokemonApiService {
 
   constructor(public http: HttpClient) { }
 
-  getPokemons(){
- console.log(environment)
-     return this.http.get(environment.pokUrl + '?limit=' + environment.limit);
-
+  getPokemons(): Observable<Pokemon[]> {
+     const url = environment.pokUrl + '?limit=' + environment.limit;
+     return this.http.get<IResult>(url)
+     .pipe(
+       map((res: IResult) => res.results.map(ires => new Pokemon(ires))),
+       tap((res: Pokemon[]) => console.log(res))
+       );
   }
 }
